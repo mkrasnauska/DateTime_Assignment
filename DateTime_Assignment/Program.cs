1825dad754155759c119f20ee1dd064e99dbf022
Margarita <margaritakrasnauska@gmail.com>
2023-02-22 09:55:29 +0000

    Add project files.

---
+++ DateTime_Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---------\nWelcome \nToday is:\n" + DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+            Console.WriteLine("Please Enter a number:");
+            Double YourNumber = Convert.ToDouble(Console.ReadLine());
+            DateTime YourDateTime = DateTime.Now.AddHours(YourNumber);
+            Console.WriteLine("in {0} hours it will be : \n{1}", YourNumber, YourDateTime);
+            Console.ReadLine();
+        }
+    }
+}
